4a64210ec82e4fa42acad49db3f7a4530d0bae49
arrachdev <muhamadfirdausar24@gmail.com>
2026-05-23 18:58:47 +0700

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Menu{
+
+String nama;
+String kategori;
+int harga;
+
+Menu(String nama,String kategori,int harga){
+
+this.nama = nama;
+this.kategori = kategori;
+this.harga = harga;
+
+}
+
+}
